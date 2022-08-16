ff8e8c052a1608a819a3132217f9a477f35cfe50
Fatemeh Behrad <41340554+FBehrad@users.noreply.github.com>
2022-08-16 16:26:10 +0430

    feat: add segmentation

---
+++ segmentation.py
@@ -0,0 +1,105 @@
+import numpy as np
+from tensorflow.keras.optimizers import Adam
+from utils import read_img_nii, extract_dataset, create_path, save_best_model, scheduler, create_batch_of_path
+from tensorflow.keras.callbacks import LearningRateScheduler
+import time
+import yaml
+import argparse
+from sklearn.model_selection import KFold
+from model import build_model
+from utils import My_Custom_Generator_segmentation, loss_gt, dice_coefficient
+
+
+def training(model, checkpoint_best, reduce_lr, data_paths, batch_size=1, epochs_per_fold=4, nb_fold=5):
+    cv = KFold(n_splits=nb_fold, shuffle=True)
+    fold = 1
+    start_time = time.time()
+    paths, labels = create_batch_of_path(data_paths)
+
+    for train, test in cv.split(data_paths):
+        train_paths = []
+        t_labels_paths = []
+        val_paths = []
+        v_labels_paths = []
+        print('Fold:  ', fold)
+        fold = fold + 1
+        for i in list(train):
+            path = paths[i]
+            train_paths.append(path)
+            label = labels[i]
+            t_labels_paths.append(label)
+        for i in list(test):
+            path = paths[i]
+            val_paths.append(path)
+            label = labels[i]
+            v_labels_paths.append(label)
+
+    my_training_batch_generator = My_Custom_Generator_segmentation(train_paths, t_labels_paths, batch_size)
+    validation_generator = My_Custom_Generator_segmentation(val_paths, v_labels_paths, batch_size)
+
+    history = model.fit(my_training_batch_generator, validation_data=validation_generator,
+                        steps_per_epoch=int(len(train_paths) // batch_size),
+                        epochs=epochs_per_fold,
+                        verbose=1,
+                        callbacks=[checkpoint_best, reduce_lr])
+
+    end_time = time.time() - start_time
+    hours, rem = divmod(end_time, 3600)
+    minutes, seconds = divmod(rem, 60)
+    print("Run time :")
+    print("{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+
+    return history
+
+
+def main():
+    dataset_path = config_file['path']['dataset']
+    augmented_path = config_file['path']['augmented_data']
+    checkpoint_path = config_file['path']['checkpoit_path']
+    best_model = config_file['path']['best_model']
+    save_model_path = config_file['path']['save_model_path']
+    extract_dataset(dataset_path, 'Segmentation')
+    extract_dataset(augmented_path, 'Augmented_Data')
+    data_paths = create_path('./Segmentation', train=True) + create_path('./Augmented_Data', aug=True)
+    input_shape = (4, 128, 128, 128)
+    output_channels = 3
+    resume_training = False
+    number_of_images = len(data_paths)
+    data = np.empty((len(data_paths[:number_of_images]),) + input_shape, dtype=np.float32)
+    labels = np.empty((len(data_paths[:number_of_images]), input_shape[1], input_shape[2], input_shape[3]),
+                      dtype=np.uint8)
+    print('data shape : ', data.shape)
+    print('labels shape : ', labels.shape)
+
+    model = build_model(input_shape=input_shape, output_channels=output_channels)
+    if bool(config_file['train_settings']['resume_training']):
+        model.load_weights(best_model)
+    checkpoint_best = save_best_model(checkpoint_path)
+    reduce_lr = LearningRateScheduler(scheduler)
+    model = build_model(input_shape=(4,128, 128, 128), output_channels=3, gradient_accumulation='True', n_gradients=8)
+    if resume_training:
+        model.load_weights(best_model)
+    model.compile(
+        optimizer=Adam(lr=float(config_file['train_settings']['lr'])),
+        loss=[loss_gt()],
+        metrics=[dice_coefficient], experimental_run_tf_function=False
+    )
+
+    history = training(model, checkpoint_best, reduce_lr,
+                       data_paths=data_paths, batch_size=int(config_file['train_settings']['train_batch_size']),
+                       epochs_per_fold=int(config_file['train_settings']['epochs_per_fold']),
+                       nb_fold=int(config_file['train_settings']['folds']))
+    model.save_weights(save_model_path)
+    return history
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config_path", "-c", help="The location of config file", default='./config.yaml')
+    args = parser.parse_args()
+    config_path = args.config_path
+    with open(config_path) as file:
+        config_file = yaml.full_load(file)
+
+    histoty = main()
---
+++ utils.py
@@ -0,0 +1,172 @@
+import zipfile
+import numpy as np
+import nibabel as nib
+import SimpleITK as sitk
+from tensorflow.keras.callbacks import ModelCheckpoint
+import glob
+import re
+from tensorflow import keras, math
+
+
+def extract_dataset(addr, target):
+    dataset_path = addr
+    zfile = zipfile.ZipFile(dataset_path)
+    zfile.extractall(target)
+
+
+def create_path(address, train=False, aug=False, val=False):
+    if train:
+        t1_pattern = address + '/*GG/*/*t1.nii.gz'
+        t2_patern = address + '/*GG/*/*t2.nii.gz'
+        flair_pattern = address + '/*GG/*/*flair.nii.gz'
+        t1ce_pattern = address + '/*GG/*/*t1ce.nii.gz'
+        seg_pattern = address + '/*GG/*/*seg.nii.gz'  # Ground truth
+    elif aug:
+        t1_pattern = address + '/*GG/*/*/*t1.nii.gz'
+        t2_patern = address + '/*GG/*/*/*t2.nii.gz'
+        flair_pattern = address + '/*GG/*/*/*flair.nii.gz'
+        t1ce_pattern = address + '/*GG/*/*/*t1ce.nii.gz'
+        seg_pattern = address + '/*GG/*/*/*seg.nii.gz'
+    elif val:
+        t1_pattern = address + '/*/*t1.nii.gz'
+        t2_patern = address + '/*/*t2.nii.gz'
+        flair_pattern = address + '/*/*flair.nii.gz'
+        t1ce_pattern = address + '/*/*t1ce.nii.gz'
+    t1 = glob.glob(t1_pattern)
+    t2 = glob.glob(t2_patern)
+    flair = glob.glob(flair_pattern)
+    t1ce = glob.glob(t1ce_pattern)
+    if not val:
+        seg = glob.glob(seg_pattern)
+    pattern = address + '/.*_(\w*)\.nii\.gz'
+    pattern = re.compile(pattern)
+    if not val:
+        data_paths = [{pattern.findall(item)[0]: item for item in items} for items in
+                      list(zip(t1, t2, t1ce, flair, seg))]
+    else:
+        data_paths = [{pattern.findall(item)[0]: item for item in items} for items in list(zip(t1, t2, t1ce, flair))]
+    return data_paths
+
+
+# def create_path_os(address, ids, val=False):
+#     data_paths = []
+#     if not
+#     add = address + '/*GG/'
+#     for i, patient_id in enumerate(ids):
+#         data_path = {}
+#         t1_add = glob.glob(add + patient_id + '/*t1.nii.gz')[0]
+#         t2_add = glob.glob(add + patient_id + '/*t2.nii.gz')[0]
+#         t1ce_add = glob.glob(add + patient_id + '/*t1ce.nii.gz')[0]
+#         flair_add = glob.glob(add + patient_id + '/*flair.nii.gz')[0]
+#         seg_add = glob.glob(add + patient_id + '/*seg.nii.gz')[0]
+#         data_path['flair'] = flair_add
+#         data_path['t1'] = t1_add
+#         data_path['t2'] = t2_add
+#         data_path['t1ce'] = t1ce_add
+#         data_path['seg'] = seg_add
+#         data_paths.append(data_path)
+
+def create_batch_of_path(data_paths):
+    labels = []
+    paths = []
+    for i, data_path in enumerate(data_paths):
+        path = []
+        path.append(data_path['flair'])
+        path.append(data_path['t1'])
+        path.append(data_path['t2'])
+        path.append(data_path['t1ce'])
+        label = data_path['seg']
+        paths.append(path)
+        labels.append(label)
+
+    return paths, labels
+
+
+def read_img_nii(img_path):
+    image_data = np.array(nib.load(img_path).get_fdata())
+    return image_data
+
+
+def read_img_sitk(img_path):
+    image_data = sitk.ReadImage(img_path)
+    return image_data
+
+
+def save_best_model(path, monitor='val_dice_coefficient', mode='max'):
+    checkpoit_best = ModelCheckpoint(path,
+                                     save_weights_only=True,
+                                     save_best_only=True,
+                                     monitor=monitor,
+                                     mode=mode,
+                                     verbose=1)
+    return checkpoit_best
+
+
+def scheduler(epoch, lr):
+    epochs = 300
+    decay = (1 - ((epoch) / epochs)) ** 0.9
+    new_lr = lr * decay
+    # print('New lr : ', new_lr)
+    return new_lr
+
+
+def dice_coefficient(y_true, y_pred):
+    intersection = math.reduce_sum(math.abs(y_true * y_pred), axis=[-3, -2, -1])
+    first_sum = math.reduce_sum(math.square(y_true), axis=[-3, -2, -1])
+    second_sum = math.reduce_sum(math.square(y_pred), axis=[-3, -2, -1])
+    dn = math.add(first_sum, second_sum)
+    epsilon = 1e-8
+    f_dn = math.add(dn, epsilon)
+    dice = math.reduce_mean(2 * intersection / f_dn)
+    return dice
+
+
+def loss_gt(e=1e-8):
+    def loss_gt_(y_true, y_pred):
+        intersection = math.reduce_sum(math.abs(y_true * y_pred), axis=[-3, -2, -1])
+        first_sum = math.reduce_sum(math.square(y_true), axis=[-3, -2, -1])
+        second_sum = math.reduce_sum(math.square(y_pred), axis=[-3, -2, -1])
+        dn = math.add(first_sum, second_sum)
+        epsilon = 1e-8
+        f_dn = math.add(dn, epsilon)
+        dice = math.reduce_mean(2 * intersection / f_dn)
+        return 1 - dice
+
+    return loss_gt_
+
+
+class My_Custom_Generator_segmentation(keras.utils.Sequence):
+
+    # custom generator to read a batch of data
+    def __init__(self, data_paths, label_paths, batch_size):
+        self.data_paths = data_paths
+        self.label_paths = label_paths
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return (np.ceil(len(self.data_paths) / float(self.batch_size))).astype(int)
+
+    def __getitem__(self, idx):
+
+        batch_x = self.data_paths[idx * self.batch_size: (idx + 1) * self.batch_size]
+        batch_y = self.label_paths[idx * self.batch_size: (idx + 1) * self.batch_size]
+
+        items = []
+        for item in batch_x:
+            flair_img = read_img_nii(item[0])
+            t1_img = read_img_nii(item[1])
+            t2_img = read_img_nii(item[2])
+            t1ce_img = read_img_nii(item[3])
+            data = np.array([t1_img, t2_img, t1ce_img, flair_img], dtype=np.float32)
+            items.append(data)
+
+        labels = []
+        for label_address in batch_y:
+            label = read_img_nii(label_address)
+            ncr = label == 1  # Necrotic and Non-Enhancing Tumor (NCR/NET)
+            ed = label == 2  # Peritumoral Edema (ED)
+            et = label == 4  # GD-enhancing Tumor (ET)
+            y = np.array([ncr, ed, et], dtype=np.float32)
+            labels.append(y)
+
+        return np.array(items), np.array(labels)
