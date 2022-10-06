3a501710de0d8811a26ac963676c866a06e01a0f
Fatemeh Behrad <41340554+FBehrad@users.noreply.github.com>
2022-10-06 20:08:51 +0330

    fix: fix bugs

--- segmentation/segmentation.py
+++ segmentation/segmentation.py
@@ -1,21 +1,18 @@
-import numpy as np
 from tensorflow.keras.optimizers import Adam
-from utils import read_img_nii, extract_dataset, create_path, save_best_model, scheduler, create_batch_of_path
-from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.callbacks import LearningRateScheduler, TensorBoard
 import time
 import yaml
-import argparse
 from sklearn.model_selection import KFold
+import datetime
+from utils import create_batch_of_path, my_custom_generator_segmentation, create_path, save_best_model, scheduler, loss_gt, dice_coefficient
 from model import build_model
-from utils import My_Custom_Generator_segmentation, loss_gt, dice_coefficient
 
 
-def training(model, checkpoint_best, reduce_lr, data_paths, batch_size=1, epochs_per_fold=4, nb_fold=5):
+def training(model, callbacks, data_paths, batch_size=1, epochs_per_fold=4, nb_fold=5):
     cv = KFold(n_splits=nb_fold, shuffle=True)
     fold = 1
     start_time = time.time()
     paths, labels = create_batch_of_path(data_paths)
-
     for train, test in cv.split(data_paths):
         train_paths = []
         t_labels_paths = []
@@ -34,14 +31,14 @@ def training(model, checkpoint_best, reduce_lr, data_paths, batch_size=1, epochs
             label = labels[i]
             v_labels_paths.append(label)
 
-        my_training_batch_generator = My_Custom_Generator_segmentation(train_paths, t_labels_paths, batch_size)
-        validation_generator = My_Custom_Generator_segmentation(val_paths, v_labels_paths, batch_size)
+        my_training_batch_generator = my_custom_generator_segmentation(train_paths, t_labels_paths, batch_size)
+        validation_generator = my_custom_generator_segmentation(val_paths, v_labels_paths, batch_size)
 
         history = model.fit(my_training_batch_generator, validation_data=validation_generator,
-                        steps_per_epoch=int(len(train_paths) // batch_size),
-                        epochs=epochs_per_fold,
-                        verbose=1,
-                        callbacks=[checkpoint_best, reduce_lr])
+                            steps_per_epoch=int(len(train_paths) // batch_size),
+                            epochs=epochs_per_fold,
+                            verbose=1,
+                            callbacks=callbacks)
 
     end_time = time.time() - start_time
     hours, rem = divmod(end_time, 3600)
@@ -53,51 +50,39 @@ def training(model, checkpoint_best, reduce_lr, data_paths, batch_size=1, epochs
 
 
 def main():
-    dataset_path = config_file['path']['dataset']
-    augmented_path = config_file['path']['augmented_data']
-    checkpoint_path = config_file['path']['checkpoit_path']
-    best_model = config_file['path']['best_model']
-    save_model_path = config_file['path']['save_model_path']
-    extract_dataset(dataset_path, 'Segmentation')
-    extract_dataset(augmented_path, 'Augmented_Data')
-    data_paths = create_path('./Segmentation', train=True) + create_path('./Augmented_Data', aug=True)
-    input_shape = (4, 128, 128, 128)
-    output_channels = 3
-    resume_training = False
-    number_of_images = len(data_paths)
-    data = np.empty((len(data_paths[:number_of_images]),) + input_shape, dtype=np.float32)
-    labels = np.empty((len(data_paths[:number_of_images]), input_shape[1], input_shape[2], input_shape[3]),
-                      dtype=np.uint8)
-    print('data shape : ', data.shape)
-    print('labels shape : ', labels.shape)
+    path = open('/content/config.yaml', 'r')
+    config = yaml.safe_load(path)
+    model_param = config['model']
+    input_size = config['preprocessing_seg']['optimal_roi']
+    input_size = (4, input_size[0], input_size[1], input_size[2])
 
-    model = build_model(input_shape=input_shape, output_channels=output_channels)
-    if bool(config_file['train_settings']['resume_training']):
-        model.load_weights(best_model)
-    checkpoint_best = save_best_model(checkpoint_path)
+    data_paths = create_path('/content/preprocessed_data', train=True) + create_path('/content/augmented_data', aug=True)
+    model = build_model(input_shape=input_size,
+                        gradient_accumulation=model_param['accumulated_grad']['enable'],
+                        n_gradients=model_param['accumulated_grad']['num_batch'])
+
+    if bool(config['training']['resume_training']):
+        model.load_weights(config['path']['best_model']).expect_partial()
+
+    checkpoint_best = save_best_model(config['path']['checkpoint'])
     reduce_lr = LearningRateScheduler(scheduler)
-    model = build_model(input_shape=(4,128, 128, 128), output_channels=3, gradient_accumulation='True', n_gradients=8)
+    log_dir = "/content/logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+
     model.compile(
-        optimizer=Adam(lr=float(config_file['train_settings']['lr'])),
+        optimizer=Adam(lr=float(config['training']['lr'])),
         loss=[loss_gt()],
         metrics=[dice_coefficient], experimental_run_tf_function=False
     )
 
-    history = training(model, checkpoint_best, reduce_lr,
-                       data_paths=data_paths, batch_size=int(config_file['train_settings']['train_batch_size']),
-                       epochs_per_fold=int(config_file['train_settings']['epochs_per_fold']),
-                       nb_fold=int(config_file['train_settings']['folds']))
-    model.save_weights(save_model_path)
+    history = training(model, [checkpoint_best, reduce_lr, tensorboard_callback],
+                       data_paths=data_paths, batch_size=int(config['training']['batch_size']),
+                       epochs_per_fold=int(config['training']['epochs_per_fold']),
+                       nb_fold=int(config['training']['number_fold']))
+    model.save_weights(config['path']['last_model'])
     return history
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--config_path", "-c", help="The location of config file", default='./config.yaml')
-    args = parser.parse_args()
-    config_path = args.config_path
-    with open(config_path) as file:
-        config_file = yaml.full_load(file)
-
     histoty = main()
+
