35d7d7a9947566719569509d5aec03b02054a712
Fatemeh Behrad <41340554+FBehrad@users.noreply.github.com>
2022-10-07 11:59:25 +0330

    refactor: change get ids

--- segmentation/post_process.py
+++ segmentation/post_process.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import SimpleITK as sitk
 import numpy as np
-from utils import extract_dataset, create_path, read_img_sitk
+from utils import read_img_sitk
 import os
 
 
-def replace_enhancing_tumor(numpy_label):
+def replace_enhancing_tumor(label):
     """
     In this function, we replace enhancing tumor with necrosis when the volume of predicted enhancing tumor is less than the threshold
+    label: numpy array
     """
-    ncr = numpy_label == 1
+    ncr = label == 1
     ncr = np.array(ncr, dtype=np.float32)
-    ed = numpy_label == 2
+    ed = label == 2
     ed = np.array(ed, dtype=np.float32)
-    et = numpy_label == 4
+    et = label == 4
     et = np.array(et, dtype=np.float32)
     mask = np.zeros_like(et)
     mask[et > 0] = 1  # replacing enhancing tumor with necrosis based on our threshold
@@ -22,12 +23,15 @@ def replace_enhancing_tumor(numpy_label):
     return mask
 
 
-def main(config_file):
-    dataset_path = config_file['path']['dataset']
-    extract_dataset(dataset_path, 'Validation')
-    data_paths = create_path('./Validation', val=True)
+if __name__ == '__main__':
+
+    parent = '../post_processed'
+    os.mkdir(parent)
+    masks = os.listdir('../submissions')
+
     # read results provided by model
-    p_results = pd.read_csv("./Stats_Validation_final.csv")
+    p_results = pd.read_csv("../Stats_Validation_final.csv")
+    
     ids = p_results.loc[:65, "Label"]
 
     et_zero_idices = []  # patients whose dice_et is 0 (false positive)
@@ -37,7 +41,9 @@ def main(config_file):
 
     et_volumes = []
     for i, patient_id in enumerate(ids):
-        label = read_img_sitk(data_paths[i]['seg'])
+        print(i)
+        print(patient_id)
+        label = read_img_sitk(os.path.join('/content/submissions', masks[i]))
         np_label = sitk.GetArrayFromImage(label)
         et = np_label == 4
         et = np.array(et, dtype=np.float32)
@@ -49,17 +55,13 @@ def main(config_file):
         if 40 >= et_volumes[i]:
             replace_et.append(ids[i])
 
-    parent = '/content/submissions/Postprocessed'
-    os.makedirs(parent)
-
     for i, patient_id in enumerate(ids):
-        label = read_img_sitk(data_paths[i]['seg'])
+        label = read_img_sitk(os.path.join('../submissions', masks[i]))
         if patient_id in replace_et:
             np_label = sitk.GetArrayFromImage(label)
             new_np_label = replace_enhancing_tumor(np_label)
             new_label = sitk.GetImageFromArray(new_np_label)
         else:
             new_label = label
-
         path = parent + '/' + patient_id + '.nii.gz'
         sitk.WriteImage(new_label, path)
