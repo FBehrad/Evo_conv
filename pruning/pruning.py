e3d47bca8adebcfe10f9923df5273b22203caaa8
Fatemeh Behrad <41340554+FBehrad@users.noreply.github.com>
2022-10-13 14:27:41 +0330

    fix: fix minor bugs

--- pruning/pruning.py
+++ pruning/pruning.py
@@ -1,56 +1,18 @@
-from tensorflow import math
-import math
 import time
 import numpy as np
-from utils import extract_dataset, create_path
-from model import build_model
+from segmentation.model import build_model
 from genetic_algorithm import wise_initialization, weighted_sum, random_initialization, evaluate_popualtion, pareto
 from genetic_algorithm import final_ranked_fitness, selection, one_point_cross_over, replacement
 import pickle
 from tensorflow.keras.optimizers import Adam
-from utils import my_custom_generator_segmentation, loss_gt, dice_coefficient
+from segmentation.utils import my_custom_generator_segmentation, loss_gt, dice_coefficient, create_path
 from keras_surgeon import delete_channels
+import yaml
+from utils import order_layers, creat_pair_path
+import tensorflow as tf
 
 
-# Order layers for pruning
-
-def order_layers(model):
-    conv_layers_indexes = []
-    layers_dic = {}
-    unordered_layers = {}
-
-    for i in range(len(model.layers)):
-        if model.layers[i].__class__.__name__ == 'Conv3D':
-            conv_layers_indexes.append(i)
-
-    for i in conv_layers_indexes:  # i = 0 3 6
-        weight = model.layers[i].get_weights()[0]
-        filter_shape = weight.shape
-        nb_of_fils = filter_shape[-1]
-        weight_dict = {}
-        # computation of l2 norm for a wise initialization
-        for j in range(nb_of_fils):
-            square = np.square(abs(weight[:, :, :, :, j]))
-            sum = np.sum(square)
-            l2 = math.sqrt(sum)
-            # l1 = np.sum(abs(weight[:,:,:,:,j])) # change this because not all networks have this dimension
-            weight_dict[j] = l2.numpy()
-
-        weight_dict_sorted = sorted(weight_dict.items(), key=lambda kv: kv[1])
-        layers_dic[i] = weight_dict_sorted
-        print('Layer ' + str(i))
-        print(weight_dict_sorted)
-
-    for x in layers_dic:
-        ordered_filters = layers_dic[x]
-        lowest_value = ordered_filters[0][-1]  # ordered_filters[0] -->(28, 26.955196)
-        unordered_layers[x] = lowest_value
-
-    ordered_layers = sorted(unordered_layers.items(), key=lambda kv: kv[1])
-    return ordered_layers, layers_dic
-
-
-def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_threshold, gen_threshold, validation_generator):
+def pruning(model, target, layers_dic, population_size, conf, validation_generator):
     filters, biases = model.layers[target].get_weights()
     w = filters.shape
     chromosome_len = w[-1]
@@ -61,7 +23,8 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
     wise_pop = wise_initialization(chromosome_len, population_size, fil, layers_dic, target)
     rand_pop = random_initialization(chromosome_len, population_size, fil)
     pop = wise_pop + rand_pop  # this is the final mask to remove filters
-    evaluated_pop = evaluate_popualtion(population_size, pop, model, target)
+    evaluated_pop = evaluate_popualtion(population_size, pop, model, target, validation_generator)
+    
     # loop
     itr = 0
     num_gen = 0
@@ -71,11 +34,10 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
     pop = evaluated_pop
     print('Lets start evolution')
 
-    while (num_gen != gen_threshold) and (itr < itr_threshold):
+    while (num_gen != conf['gen_threshold']) and (itr < conf['itr_threshold']):
         print('---------------------------------------------------------------------')
         print('Iteretion : ', itr + 1)
         # Find best chromosome
-        print('pop', pop)
         b_indices = pareto(pop, population_size)
         # print('B_indices',b_indices)
         best_choromosome_idx = b_indices[0]
@@ -86,16 +48,16 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
             best_results.append(best_chromosome[1])
         print('Best chromosome : ', best_chromosome)
         # Rank chromosomes
-        ranked_chromosome = weighted_sum(w1, w2, population_size, b_indices, pop)
+        ranked_chromosome = weighted_sum(conf['w1'], conf['w2'], population_size, b_indices, pop)
         # print('Ranked chromosomes',ranked_chromosome)
 
         final_fitnesses = final_ranked_fitness(population_size, b_indices, ranked_chromosome)
 
         # Selection
         selected_pop = selection(population_size, final_fitnesses)
-
+        
         # Crossover
-        children = one_point_cross_over(population_size, selected_pop, pc, pop, chromosome_len)
+        children = one_point_cross_over(population_size, selected_pop, conf['pc'], pop, chromosome_len)
 
         # Mutation
         # final_children = mutation(children,pm,chromosome_len)
@@ -105,7 +67,7 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
         evaluated_children = evaluate_popualtion(population_size, children, model, target, validation_generator)
 
         # Replacement
-        next_generation = replacement(evaluated_children, pop, population_size, w1, w2)
+        next_generation = replacement(evaluated_children, pop, population_size, conf['w1'], conf['w2'])
         # print('Next gen: ',next_generation )
         best_member = next_generation[0]
         # print('Best member of next gen: ', best_member )
@@ -117,7 +79,7 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
             print('Num gen:', num_gen)
         else:
             num_gen = 0
-            print('Num_gen‌: ', num_gen)
+            print('Num gen: ', num_gen)
 
         pop = next_generation
         itr = itr + 1
@@ -132,72 +94,87 @@ def pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_thre
     return best_members[-1], best_members
 
 
-def main(config_file):
-    dataset_path = config_file['path']['dataset']
-    extract_dataset(dataset_path, 'Data')
-    data_paths = create_path('./Data', train=True)
-    best_model = config_file['path']['best_model']
-
-    labels_val = []
-    paths_val = []
-    for i, data_path in enumerate(data_paths):
-        path = []
-        path.append(data_path['flair'])
-        path.append(data_path['t1'])
-        path.append(data_path['t2'])
-        path.append(data_path['t1ce'])
-        label = data_path['seg']
-        paths_val.append(path)
-        labels_val.append(label)
-
-    batch_size = 1
-    validation_generator = my_custom_generator_segmentation(paths_val, labels_val, batch_size)
-
-    model = build_model(input_shape=(4, 128, 128, 128), output_channels=3, gradient_accumulation='True', n_gradients=8)
-
-    model.load_weights(best_model)
-
-    # for i, layer in enumerate(model.layers):
-    #     print('layer:', i, '\tName:', layer.name, '\tClass:', layer.__class__.__name__)
-
-
-    w1 = 0.8  # weight for accuracy
-    w2 = -0.2  # weight for run time
-    pc = 0.9  # higher cross over probability --> more exploitation
-    pm = 0.1  # higher mutation probability --> more exploration
-    itr_threshold = 25
-    gen_threshold = 4  # if the best member didn't change, this parameter would stop the algorithm
-    layers = [5, 12]
-    nb_filters = [32, 128]
-    ratio = 0.25
+def genetic_pruning(config, layers, nb_filters, model, data_paths, ratio):
+    # create generators
+    paths_val, labels_val = creat_pair_path(data_paths)
+    validation_generator = my_custom_generator_segmentation(paths_val, labels_val, config['training']['batch_size'])
 
     ordered_layers, layers_dic = order_layers(model)  # put your model name here
     final_layers = {}
-    # for i in range(len(ordered_layers)):
-    #   layers.append(ordered_layers[i][0])
     best_over_gens = []
 
     for i, target in enumerate(layers):
         population_size = int((ratio * nb_filters[i]))
         print('Target => ', target)
-        final_layer, best_over_gen = pruning(model, target, layers_dic, population_size, w1, w2, pc, pm, itr_threshold,
-                                             gen_threshold, validation_generator)
-        final_layers[target] = final_layer
+        final_layer, best_over_gen = pruning(model, target, layers_dic, population_size, config['genetic'],
+                                             validation_generator)
         best_over_gens.append(best_over_gen)
+        final_layers[target] = final_layer
         print('Target:', target, 'Best over generation', best_over_gen)
 
-    # final_layers
-    with open('/content/drive/MyDrive/final_layers_64_64layers', 'wb') as fp:
-        pickle.dump(final_layers, fp)
+    return final_layers
 
+
+def remove_filters(model, final_layers):
     new_model = model
     for key in final_layers:
         best_mask = final_layers[key][0]
         indices = np.where(best_mask < 1)
         removed_filters_indices = indices[0].tolist()  # indices of filters which will be removed
         conv_layer = new_model.get_layer(index=key)
-        new_model = delete_channels(new_model, conv_layer,removed_filters_indices)
+        new_model = delete_channels(new_model, conv_layer, removed_filters_indices)
         new_model.compile(optimizer=Adam(lr=1e-4), loss=[loss_gt()], metrics=[dice_coefficient],
                           experimental_run_tf_function=False)
 
     return new_model
+
+
+if __name__ == '__main__':
+    path = open('../config.yaml', 'r')
+    config = yaml.safe_load(path)
+    data_paths = create_path('../preprocessed_data', train=True) + create_path('../augmented_data', aug=True)
+
+    # load model
+    best_model = config['path']['best_model']
+    input_size = config['preprocessing_seg']['optimal_roi']
+    input_size = (4, input_size[0], input_size[1], input_size[2])
+    model_param = config['model']
+    model = build_model(input_shape=input_size, gradient_accumulation=model_param['accumulated_grad']['enable'],
+                        n_gradients=model_param['accumulated_grad']['num_batch'])
+    model.load_weights(best_model).expect_partial()
+
+    # The indices of convolutional layers which can be pruned and the number of their filters
+    layers = [5, 13, 94, 20, 64, 43, 28, 74, 35, 84]
+    nb_filters = [32, 64, 32, 64, 256, 256, 128, 128, 128, 64]
+    all_layers = genetic_pruning(config, layers, nb_filters, model, data_paths, ratio=0.25)
+
+    if config['genetic']['version'] == 'third':
+        layers = [50, 57]
+        nb_filters = [256, 256]
+        final_layers = genetic_pruning(config, layers, nb_filters, model, data_paths, ratio=0.5)
+    else:
+        layers = [50, 57]
+        nb_filters = [256, 256]
+        final_layers = genetic_pruning(config, layers, nb_filters, model, data_paths, ratio=0.25)
+
+    all_layers = all_layers + final_layers
+
+    # Final_layers
+    with open('../final_layers/final_layers', 'wb') as fp:
+        pickle.dump(all_layers, fp)
+
+    pruned_model = remove_filters(model, all_layers)
+
+    # Prepare a model for validation
+    val_model = build_model(input_shape=(4, 240, 240, 160),
+                            gradient_accumulation=model_param['accumulated_grad']['enable'],
+                            n_gradients=model_param['accumulated_grad']['num_batch'])
+
+    pruned_val_model = remove_filters(val_model, all_layers)
+    pruned_val_model.set_weights(pruned_model.get_weights())
+
+    if config['genetic']['version'] == 'third':
+        tf.keras.models.save_model(pruned_val_model, '../Pruned_a_third_model.h5')
+    else:
+        tf.keras.models.save_model(pruned_val_model, '../Pruned_a_forth_model.h5')
+
